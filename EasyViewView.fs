db3feef826ae238983f43015e29639a5d6552640
Speedelfe <trautmann.n@gmail.com>
2020-05-09 14:08:09 +0200

    Headline zu EasyView hinzu

--- EasyViewView.fs
+++ EasyViewView.fs
@@ -39,16 +39,49 @@ module EasyViewView =
             Border.child view
         ]
 
+    let getHeadline () : IView list =
+        [
+            TextBlock.create [
+                TextBlock.column 0
+                TextBlock.row 0
+                TextBlock.text "Für"
+            ]
+            TextBlock.create [
+                TextBlock.column 1
+                TextBlock.row 0
+                TextBlock.text "gepl. Ausgabe"
+            ]
+            TextBlock.create [
+                TextBlock.column 2
+                TextBlock.row 0
+                TextBlock.text "Geschenk"
+            ]
+            TextBlock.create [
+                TextBlock.column 3
+                TextBlock.row 0
+                TextBlock.text "Kosten"
+            ]
+            TextBlock.create [
+                TextBlock.column 4
+                TextBlock.row 0
+                TextBlock.text "Besorgt?"
+            ]
+            TextBlock.create [
+                TextBlock.column 5
+                TextBlock.row 0
+                TextBlock.text "Besorgt von"
+            ]
+        ]
     let giftEmpfaengerView (index:int, plannedGift:PlannedGift): IView list =
         [
             TextBlock.create [
                 TextBlock.column 0
-                TextBlock.row index
+                TextBlock.row (index + 1)
                 TextBlock.text plannedGift.person.name
             ]
             TextBlock.create [
                 TextBlock.column 1
-                TextBlock.row index
+                TextBlock.row (index + 1)
                 let euroSymbol = "€"
                 match plannedGift.person.plannedExpenses with
                 | Some expenses -> TextBlock.text ((expenses |> string) + " " + euroSymbol)
@@ -56,25 +89,25 @@ module EasyViewView =
             ]
             TextBlock.create [
                 TextBlock.column 2
-                TextBlock.row index
+                TextBlock.row (index + 1)
                 TextBlock.text plannedGift.gift.description
             ]
             TextBlock.create [
                 TextBlock.column 3
-                TextBlock.row index
+                TextBlock.row (index + 1)
                 let euroSymbol = "€"
                 TextBlock.text ((plannedGift.gift.totalCost |> string) + " " + euroSymbol)
             ]
             CheckBox.create [
                 CheckBox.column 4
-                CheckBox.row index
+                CheckBox.row (index + 1)
                 match plannedGift.purchaseStatus with
                 | Some state -> CheckBox.isChecked state.alreadyBought
                 | None -> CheckBox.isChecked false
             ]
             TextBlock.create [
                 TextBlock.column 5
-                TextBlock.row index
+                TextBlock.row (index + 1)
                 match plannedGift.purchaseStatus with
                 | Some buyer -> (TextBlock.text buyer.whoBuys)
                 | None -> (TextBlock.text "")
@@ -92,14 +125,17 @@ module EasyViewView =
                 Grid.create [
                     Grid.dock Dock.Left
                     Grid.columnDefinitions "1*, 1*, 1*, 1*, 1*, 1*"
-                    Grid.rowDefinitions (
-                        giftList
-                        |> List.map (fun _ -> "Auto")
-                        |> List.reduce (fun a b -> a + "," + b))
-                    Grid.children (
-                        giftList
-                        |> List.indexed
-                        |> List.collect giftEmpfaengerView)
+                    Grid.rowDefinitions ("Auto," +
+                            (
+                                giftList
+                                |> List.map (fun _ -> "Auto")
+                                |> List.reduce (fun a b -> a + "," + b)
+                            )
+                        )
+                    Grid.children (List.concat [
+                        getHeadline()
+                        giftList |> List.indexed |> List.collect giftEmpfaengerView
+                    ])
                 ]
             ]
         ]
