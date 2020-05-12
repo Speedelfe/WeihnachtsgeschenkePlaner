81bd89531c2e8fe727985c9d185f61dedac5706d
Speedelfe <trautmann.n@gmail.com>
2020-05-12 18:12:25 +0200

    Ansicht erweitert mit dynamischen Spalten aus dem SPlit

--- NotEasyViewView.fs
+++ NotEasyViewView.fs
@@ -84,7 +84,21 @@ module NotEasyViewView =
             TextBlock.create [
                 TextBlock.column 8
                 TextBlock.row 0
-                TextBlock.text "Anteil von"
+                TextBlock.text "Anteil Betrag"
+            ]
+        ]
+
+    let anteilTextBlock (split:Split) index prevColm :IView list =
+        [
+            TextBlock.create [
+                TextBlock.column (prevColm + 1)
+                TextBlock.row (index + 1)
+                TextBlock.text (getPersonNameValue split.involvedPerson |> string)
+            ]
+            TextBlock.create [
+                TextBlock.column (prevColm + 2)
+                TextBlock.row (index + 1)
+                TextBlock.text (split.splitAmount |> string)
             ]
         ]
 
@@ -94,7 +108,7 @@ module NotEasyViewView =
                 TextBlock.column 0
                 TextBlock.row (index + 1)
                 TextBlock.text (getPersonNameValue plannedGift.receiver)
-            ]
+            ] :> IView
             TextBlock.create [
                 TextBlock.column 1
                 TextBlock.row (index + 1)
@@ -108,18 +122,18 @@ module NotEasyViewView =
                     | Some { plannedExpenses = None }
                     | None -> ""
                 |> TextBlock.text
-            ]
+            ] :> IView
             TextBlock.create [
                 TextBlock.column 2
                 TextBlock.row (index + 1)
                 TextBlock.text plannedGift.gift.description
-            ]
+            ] :> IView
             TextBlock.create [
                 TextBlock.column 3
                 TextBlock.row (index + 1)
                 let euroSymbol = "€"
                 TextBlock.text ((plannedGift.gift.totalCost |> string) + " " + euroSymbol)
-            ]
+            ] :> IView
             CheckBox.create [
                 CheckBox.column 4
                 CheckBox.row (index + 1)
@@ -129,8 +143,7 @@ module NotEasyViewView =
                     | Some state -> state.alreadyBought
                     | None -> false
                 )
-
-            ]
+            ] :> IView
             TextBlock.create [
                 TextBlock.column 5
                 TextBlock.row (index + 1)
@@ -138,7 +151,7 @@ module NotEasyViewView =
                     match plannedGift.purchaseStatus with
                     | Some buyer -> (getPersonNameValue buyer.whoBuys)
                     | None ->  "")
-            ]
+            ] :> IView
             TextBlock.create [
                 TextBlock.column 6
                 TextBlock.row (index + 1)
@@ -151,8 +164,17 @@ module NotEasyViewView =
                     |> (fun sumOtherAmounts -> plannedGift.gift.totalCost - sumOtherAmounts |> string)
                     |> (fun selfAmount -> selfAmount + " " + euroSymbol)
                     |> TextBlock.text
-            ]
+            ] :> IView
         ]
+        |> fun list ->
+            // Textblöcke für Anteile
+            let mutable column = 6
+            plannedGift.splitList
+            |> List.collect (fun split ->
+                let result = anteilTextBlock split index column
+                column <- (column + 2)
+                result )
+            |> List.append list
 
     let notEasyView (giftList: PlannedGift list) personList dispatch =
         DockPanel.create [
@@ -160,13 +182,13 @@ module NotEasyViewView =
             DockPanel.children [
                 TextBlock.create [
                     TextBlock.dock Dock.Top
-                    TextBlock.text "Einfache Planungs Ansicht"
+                    TextBlock.text "Ausführliche Planungs Ansicht"
                 ]
                 Grid.create [
                     Grid.dock Dock.Left
                     Grid.columnDefinitions ("1*, 1*, 1*, 1*, 1*, 1*, 1*" +
                         (
-                            (getMostSplittedGift giftList, ",1*")
+                            (getMostSplittedGift giftList, ",1*,1*")
                             ||> String.replicate
                         )
                     )
